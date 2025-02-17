9ebd3e7ddd4aa51953f51e97ccece200408d95a3
kuaworu <jushinskaja.n@gmail.com>
2025-02-17 15:54:05 +0200

    letters_counter.py

---
+++ letters_counter.py
@@ -0,0 +1,9 @@
+# # Функция для подсчёта количества каждой буквы в слове
+# def count_letters(word):
+#     letter_count = {}
+#     for letter in word:
+#         if letter in letter_count:
+#             letter_count[letter] += 1
+#         else:
+#             letter_count[letter] = 1
+#     return letter_count
