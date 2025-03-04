650e96ef56c27db2034fced2debebb3177f0b721
kuaworu <jushinskaja.n@gmail.com>
2025-03-04 20:54:05 +0200

    Delete bookcreator.py

--- bookcreator.py
+++
@@ -1,8 +0,0 @@
-def guestBook():
-    name = input("sinu nimi: ")
-    comment = input("sinu sõnum: ")
-    text = f"nimi: {name}, sõnum: {comment}\n"
-    file = open("guestbook.txt", "a", encoding="utf-8")
-    file.write(text)
-    file.close()
-    print("sõnum oli salvestatu edukalt")
