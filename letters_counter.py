1a241a4035e49d4803640c237dd25168afebf980
kuaworu <jushinskaja.n@gmail.com>
2025-03-04 20:50:44 +0200

    Delete letters_counter.py

--- letters_counter.py
+++
@@ -1,9 +0,0 @@
-# # Функция для подсчёта количества каждой буквы в слове
-# def count_letters(word):
-#     letter_count = {}
-#     for letter in word:
-#         if letter in letter_count:
-#             letter_count[letter] += 1
-#         else:
-#             letter_count[letter] = 1
-#     return letter_count
