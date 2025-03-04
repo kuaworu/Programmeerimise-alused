2d82e9830a2d7e89c7584abd0ecd36ac49900c07
kuaworu <jushinskaja.n@gmail.com>
2025-03-04 20:50:19 +0200

    Delete search_element.py

--- search_element.py
+++
@@ -1,6 +0,0 @@
-# Функция для поиска элемента в кортеже
-def search_element(tpl, element):
-    if element in tpl:
-        return tpl.index(element)  # Возвращаем индекс элемента, если он найден
-    else:
-        return -1  # Возвращаем -1, если элемент не найден
