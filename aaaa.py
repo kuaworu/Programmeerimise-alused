55dcabebccdc36a641d62b10a59c4bf687213115
kuaworu <jushinskaja.n@gmail.com>
2025-05-07 11:14:48 +0300

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -37,27 +37,56 @@ import datetime
 #         
 # võimalus vaadata kõik protsessid arvutiss
 # salvestada need failisse tasklist ["Time", "Proccess name", "Memory usage"]
-import os
+# импорт нужных библиотек
+hosts = ["8.8.8.8", "1.1.1.1", "192.168.56.1"]
 
-with open("taskid.csv","w",newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(["Time","Proccess name","Memory Usage"])
-
-# Running the aforementioned command and saving its output
-output = os.popen("tasklist").read()
-test = output.splitlines()
-for i in range(7,len(test)):
-    now = datetime.datetime.now()
-    proccesName = test[i].split()
-    name = proccesName[0]
-    memory = proccesName[-2]
-    
-    with open("taskid.csv","a",newline="") as file:
+def checkconection():
+    with open("ping_log.csv", "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([now,name,memory])
+        writer.writerow(["Time", "Status"])
 
-    print("time: ",now, "name: ", name,"memory: ", memory )
-    
-# Displaying the output
+    while True:
+        print("kättesadavuse kontroll")
+        now = datetime.datetime.now()
+        result = ""
+        for elem in hosts:
+            response = os.system(f"ping -n 1 {elem} > nul")
+            if response == 0:
+                result = "OK"
+                print(elem, "kätesadavalt")
+            else:
+                result = "Fail"
+                print(elem, "ei ole kättesadavalt")
 
+            with open("ping_log.csv", "a", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow([now, result])
 
+            print("-" * 30)
+            time.sleep(5)
+
+def taskScaner():
+    with open("taskid.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Time", "Proccess name", "Memory Usage"])
+
+    output = os.popen("tasklist").read()
+    test = output.splitlines()
+    for i in range(7, len(test)):
+        now = datetime.datetime.now()
+        proccesName = test[i].split()
+        if len(proccesName) < 5:
+            continue
+        name = proccesName[0]
+        memory = proccesName[-2]
+
+        with open("taskid.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([now, name, memory])
+
+        print("time: ", now, "name: ", name, "memory: ", memory)
+
+if int(input("sisesta number ")) == 1:
+    checkconection()
+else:
+    taskScaner()
