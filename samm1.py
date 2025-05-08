16e5d940e354eaa0273d847345eaa079ee32b124
kuaworu <jushinskaja.n@gmail.com>
2025-05-08 13:41:13 +0300

    Salvestatud nimekiri staatusega 'alive' või 'dead

--- samm1.py
+++ samm1.py
@@ -1,4 +1,11 @@
+import random
+
 with open("characters.txt", "r", encoding="utf-8") as file:
     characters = [line.strip() for line in file.readlines()]
-    
-print(characters)
+
+with open("output.txt", "w", encoding="utf-8") as out:
+        for character in characters:
+            status = random.choice(["alive", "dead"])
+            out.write(f"{character} - {status}\n")
+            
+print("output.txt created with character statuses")
